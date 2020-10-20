3a9e97bba9ed2d3814e2d6ca57a3a9cee84ba870
Julien Bronner <julbronner@free.fr>
2020-10-20 11:52:10 +0200

    add decoupage_donne

--- TD1.py
+++ TD1.py
@@ -6,7 +6,7 @@ Created on Tue Oct 20 10:26:18 2020
 """
 import pickle
 import numpy as np
-from random import randint
+from random import sample
 
 def unpickle(file):
     with open(file, 'rb') as fo:
@@ -21,4 +21,11 @@ def lecture_cifar(path):
     return data_X, label_Y
 
 def decoupage_donnes(X, Y):
-    
+    n = len(X)
+    nbr_aleatoires_test = sample(range(0,n), int(n/5))
+    nbr_app = list(set(range(0,n)) - set(nbr_aleatoires_test))
+    Xtest = np.take(X, nbr_aleatoires_test, 0)
+    Ytest = np.take(Y, nbr_aleatoires_test, 0)
+    Xapp = np.take(X, nbr_app, 0)
+    Yapp = np.take(Y, nbr_app, 0)
+    return Xapp, Yapp, Xtest, Ytest
