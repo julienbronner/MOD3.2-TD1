9ff4ead7b9daad116cfa65c03ab94ade068980b1
Julien Bronner <julbronner@free.fr>
2020-10-20 11:28:51 +0200

    first commit

---
+++ TD1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 20 10:26:18 2020
+
+@author: julbr
+"""
+import pickle
+import numpy as np
+from random import randint
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def lecture_cifar(path):
+    dico_tot = unpickle(path)
+    data_X = np.array(dico_tot[b'data'])
+    data_X = data_X.astype(float)
+    label_Y = np.array(dico_tot[b'labels'])
+    return data_X, label_Y
+
+def decoupage_donnes(X, Y):
+    
