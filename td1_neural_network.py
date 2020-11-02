29ed464f494b39f7ba4ce75c36fab58394398a29
Raphaël Chalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2020-11-02 10:50:20 +0100

    Maj réseau de neurones avant le TD2

--- td1_neural_network.py
+++ td1_neural_network.py
@@ -9,9 +9,10 @@ Created on Tue Oct 20 10:40:01 2020
 #%% Imports
 import pickle
 import numpy as np
-from random import sample
+import matplotlib.pyplot as plt
 
-np.random.seed(1) # pour que l'exécution soit déterministe
+seed = 2
+np.random.seed(seed) # pour que l'exécution soit déterministe
 
 #%% Code Julien Bronner
 
@@ -28,8 +29,11 @@ def lecture_cifar(path):
     return data_X, label_Y
 
 def decoupage_donnes(X, Y):
+    np.random.seed(seed)
+    
     n = len(X)
-    nbr_aleatoires_test = sample(range(0,n), int(n/5))
+    nbr_aleatoires_test = np.random.choice(n, int(n/5), replace=False)
+    # nbr_aleatoires_test = sample(range(0,n), int(n/5))
     nbr_app = list(set(range(0,n)) - set(nbr_aleatoires_test))
     Xtest = np.take(X, nbr_aleatoires_test, 0)
     Ytest = np.take(Y, nbr_aleatoires_test, 0)
@@ -66,7 +70,6 @@ def kppv_predict(dist, Yapp, K): # utilisationde np.argpartition(A,k) qui donne 
     return ''
 
 #%% Chargement des données
-
 arbo_ia = "/home/raphael/Documents/Centrale Lyon/Apprentissage profond et IA/"
 data_X, label_Y = lecture_cifar(arbo_ia + "cifar-10-batches-py/data_batch_1")
 Xapp, Yapp, Xtest, Ytest = decoupage_donnes(data_X, label_Y)
@@ -125,35 +128,53 @@ def evaluation_classifieur(Ytest, Ypred):
     accuracy = s/n
     return accuracy
 
-#%% Génération des données
+def sigmoid(X, W):
+    I = X.dot(W) # Potentiel d'entrée de la couche
+    O = np.where(I >= 0, 1/(1 + np.exp(-I)), np.exp(I)/(1 + np.exp(I)))
+    # O = 1/(1 + np.exp(-I)) # Sortie de la couche (fonction d'activation de type sigmoïde)
+    return O
 
-# N est le nombre de données d'entrée
-# D_in est la dimension des données d'entrée
-# D_h le nombre de neurones de la couche cachée
-# D_out est la dimension de sortie (nombre de neurones de la couche de sortie)
-def neural_network_classification_cifar_10(Xapp, Yapp, gradient_step=1e-3, nb_iterations = 1000):
+def d_sigmoid(X):
+    return X*(1 - X)  
+
+def neural_network_classification_cifar_10_train(Xapp,
+                                                 Yapp,
+                                                 gradient_step=1e-3,
+                                                 nb_iterations = 1000):
+    
+    np.random.seed(seed)
+    
+    # N est le nombre de données d'entrée
+    # D_in est la dimension des données d'entrée
+    # D_h le nombre de neurones de la couche cachée
+    # D_out est la dimension de sortie (nombre de neurones de la couche de sortie)
     N, D_in = np.shape(Xapp)
     D_h, D_out = 20, 10
     
     # Création d'une matrice d'entrée X et de sortie Y
-    X = Xapp/255 #Normalisation des données
+    X = np.array(Xapp/255, dtype="float32") #Normalisation des données
     Y = matrice_stochastique(Yapp)
+    
+    # Vecteurs pour mesurer la performance du modèle
+    vect_loss = np.zeros(nb_iterations)
+    vect_accuracy = np.zeros(nb_iterations)
     
     # Initialisation aléatoire des poids du réseau
     W1 = 2*np.random.random((D_in, D_h)) - 1
-    b1 = np.zeros((1, D_h))
+    W1 = np.array(W1, dtype="float32")
+    # b1 = np.zeros((1, D_h))
     W2 = 2*np.random.random((D_h, D_out))-1
-    b2 = np.zeros((1, D_out))
+    W2 = np.array(W2, dtype="float32")
+    # b2 = np.zeros((1, D_out))
     
     for i in range(nb_iterations):
         ####################################################
         # Passe avant : calcul de la sortie prédite Y_pred #
         ####################################################
-        I1 = X.dot(W1) + b1 # Potentiel d'entrée de la couche cachée
-        O1 = 1/(1 + np.exp(-I1)) # Sortie de la couche cachée (fonction d'activation de type sigmoïde)
-        I2 = O1.dot(W2) + b2 # Potentiel d'entrée de la couche de sortie
-        O2 = 1/(1 + np.exp(-I2)) # Sortie de la couche de sortie (fonction d'activation de type sigmoïde)
-        O2 = O2/np.sum(O2, axis=1)[:,np.newaxis] # Normalisation des coefficients sur chaque ligne
+        O1 = sigmoid(X, W1) # Sortie de la couche cachée (fonction d'activation de type sigmoïde)
+        O2 = sigmoid(O1, W2) # Sortie de la couche de sortie (fonction d'activation de type sigmoïde)
+        O2 = np.divide(O2, np.sum(O2, axis=1)[:,np.newaxis], dtype="float32")
+        # O2/np.sum(O2, axis=1)[:,np.newaxis] # Normalisation des coefficients sur chaque ligne
         Y_pred = O2 # Les valeurs prédites sont les sorties de la couche de sortie
         
         # if i == 0:
@@ -162,29 +183,44 @@ def neural_network_classification_cifar_10(Xapp, Yapp, gradient_step=1e-3, nb_it
         # Calcul et affichage de la fonction perte de type MSE #
         ########################################################
         loss = np.square(Y_pred - Y).sum()/2
-        if i%int(nb_iterations/10) == 0 :
-            print(i, "loss :", loss)
+        vect_loss[i] = loss
+        # if i%int(nb_iterations/10) == 0 :
+        #     print(i, "loss :", loss)
+        
+        ######################################################
+        # Calcul et affichage de la précision du classifieur #
+        ######################################################
+        accuracy = evaluation_classifieur(Y, Y_pred)
+        vect_accuracy[i] = accuracy
         
         ########################################
         # Passe arrière : mis à jour des poids #
         ########################################
         grad_Y_pred = 2*(Y_pred - Y)
-        grad_O2 = grad_Y_pred*(O2*(1 - O2))
+        grad_O2 = grad_Y_pred*d_sigmoid(O2)
         grad_w2 = O1.T.dot(grad_O2)
         
-        grad_O1 = O1*(1 - O1)
+        grad_O1 = d_sigmoid(O1)
         grad_w1 = X.T.dot((grad_O2.dot(W2.T))*grad_O1)
         
         W1 -= gradient_step*grad_w1
         W2 -= gradient_step*grad_w2
-    return Y_pred, W1, W2
+    
+    vect_accuracy*= 100
+    
+    return Y_pred, W1, W2, vect_loss, vect_accuracy
 
-def neural_network_classification_cifar_10_test(Xtest, Ytest, W1, W2, gradient_step=1e-3, nb_iterations = 1000):
+def neural_network_classification_cifar_10_test(Xtest, 
+                                                Ytest, 
+                                                W1, 
+                                                W2, 
+                                                gradient_step=1e-3, 
+                                                nb_iterations = 1000):
     N, D_in = np.shape(Xtest)
     D_h, D_out = 20, 10
     
     # Création d'une matrice d'entrée X et de sortie Y
-    X = Xtest/255 #Normalisation des données
+    X = np.array(Xtest/255, dtype="float32")  #Normalisation des données
     
     # Initialisation des poids du réseau
     b1 = np.zeros((1, D_h))
@@ -203,6 +239,11 @@ def neural_network_classification_cifar_10_test(Xtest, Ytest, W1, W2, gradient_s
     return Y_pred
 
 #%% Recherche du meilleur gradient_step et nombre d'itérations
+
+##################################################
+# ATTENTION : Met plusieurs minutes à s'exécuter #
+##################################################
+
 liste_gradient_step = [5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5]
 liste_nb_iterations = [100, 200, 500, 1000]
 nb_gradient_steps = len(liste_gradient_step)
@@ -213,7 +254,7 @@ Y = matrice_stochastique(Yapp)
 
 for i, g_step in enumerate(liste_gradient_step):
     for j, nb_iter in enumerate(liste_nb_iterations):
-        Y_pred, W1, W2 = neural_network_classification_cifar_10(Xapp, Yapp, gradient_step = g_step, nb_iterations = nb_iter)
+        Y_pred, W1, W2, vect_loss, vect_accuracy = neural_network_classification_cifar_10_train(Xapp, Yapp, gradient_step = g_step, nb_iterations = nb_iter)
         accuracy = evaluation_classifieur(Y, Y_pred)
         accuracy *= 100
         accuracy_matrix[i,j] = accuracy
@@ -229,24 +270,43 @@ for i, g_step in enumerate(liste_gradient_step):
 #        [16.3625, 20.7   , 22.35  , 24.425 ],
 #        [16.7875, 18.1   , 18.7375, 21.55  ],
 #        [11.45  , 13.425 , 16.05  , 18.425 ]])
+# The gradient steps 1e-3 and 5e-4 appears to be the best.
 
 #%% 
-
 gradient_step = 1e-3
-nb_iterations = 100
+nb_iterations = 400
 
 Yapp_stochastique = matrice_stochastique(Yapp)
-Y_pred, W1, W2 = neural_network_classification_cifar_10(Xapp, Yapp, gradient_step=gradient_step, nb_iterations=nb_iterations)
-accuracy = evaluation_classifieur(Yapp_stochastique, Y_pred)
-accuracy *= 100
-print("Gradient step :", gradient_step, "and Nb iterations :", nb_iterations)
-print("Accuracy :", round(accuracy, 2), "%")
+Y_pred, W1, W2, vect_loss, vect_accuracy = neural_network_classification_cifar_10_train(Xapp, Yapp, gradient_step=gradient_step, nb_iterations=nb_iterations)
 
-Ytest_stochastique = matrice_stochastique(Ytest)
-Ypred_test = neural_network_classification_cifar_10_test(Xtest, Ytest, W1, W2, gradient_step=gradient_step, nb_iterations=nb_iterations)
-accuracy_test = evaluation_classifieur(Ytest_stochastique, Ypred_test)
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+fig.suptitle("Fonction de perte en fonction des itérations")
+ax1.set_xlabel(r'Itération $i$')
+ax1.set_ylabel("Perte")
+# ax1.set_xticks(np.arange(0, 25, step=2))
+ax1.grid()
+ax1.plot(vect_loss)
+
+ax2 = fig.add_subplot(212)
+fig.suptitle("Précision en fonction des itérations")
+ax2.set_xlabel(r'Itération $i$')
+ax2.set_ylabel("Précision")
+# ax.set_xticks(np.arange(0, 25, step=2))
+ax2.grid()
+ax2.plot(vect_accuracy)
+
+# accuracy = evaluation_classifieur(Yapp_stochastique, Y_pred)
+# accuracy *= 100
+# print("Gradient step :", gradient_step, "and Nb iterations :", nb_iterations)
+# print("Accuracy :", round(accuracy, 2), "%")
+
+Y_test_stochastique = matrice_stochastique(Ytest)
+Y_pred_test = neural_network_classification_cifar_10_test(Xtest, Ytest, W1, W2, gradient_step=gradient_step, nb_iterations=nb_iterations)
+accuracy_test = evaluation_classifieur(Y_test_stochastique, Y_pred_test)
 accuracy_test *= 100
 print("Gradient step :", gradient_step, "and Nb iterations :", nb_iterations)
+print("Maximum accuracy :", round(np.amax(vect_accuracy), 2), "% reached at iteration ", np.where(vect_accuracy == np.amax(vect_accuracy))[0][0])
 print("Accuracy test data:", round(accuracy_test, 2), "%")
 
 
