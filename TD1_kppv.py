08d5b3b5f3a329fe1e4bf978a232af8b79b7b888
Julien Bronner <julbronner@free.fr>
2020-11-03 10:10:00 +0100

    ajout code kppv et test

--- TD1_kppv.py
+++ TD1_kppv.py
@@ -6,10 +6,13 @@ Created on Tue Oct 20 10:26:18 2020
 """
 import pickle
 import numpy as np
+from scipy import stats
 
 seed = 2
 np.random.seed(seed) # pour que l'exécution soit déterministe
+fraction_test = 1/5
 
+#%% Fonctions
 def unpickle(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
@@ -25,7 +28,7 @@ def lecture_cifar(path):
 def decoupage_donnes(X, Y):
     np.random.seed(seed)
     n = len(X)
-    nbr_aleatoires_test = np.random.choice(n, int(n/5), replace=False)
+    nbr_aleatoires_test = np.random.choice(n, int(n*fraction_test), replace=False)
     # nbr_aleatoires_test = sample(range(0,n), int(n/5))
     nbr_app = list(set(range(0,n)) - set(nbr_aleatoires_test))
     Xtest = np.take(X, nbr_aleatoires_test, 0)
@@ -53,12 +56,40 @@ def kppv_distances(Xtest, Xapp): #ça tourne mais j'espère que c'est juste x)
     dist = terme1_somme + terme2_somme - 2*terme3_somme 
     return dist
     
+def kppv_distances_bala(Xtest, Xapp):
+    a_2 = (Xtest**2).sum(axis=1)
+    a_2 = a_2.reshape((-1,1))
+
+    b_2 = (Xapp**2).sum(axis=1)
+    b_2 = b_2.reshape((1,-1))
+    
+    dist = -2 * Xtest.dot(Xapp.T) + a_2 + b_2
+
+    return dist
+
 def kppv_predict(dist, Yapp, K): # utilisationde np.argpartition(A,k) qui donne les indices pour que jusqu'à k, on ait les valeurs les  plus petites
     N,M = np.shape(dist)
     sort_indices = np.argpartition(dist, K-1, axis = 0) #K-1 car on part de 0
-    Yapp_mat = Yapp.dot(np.ones(1,M)) # pour dupliquer Yapp dans toutes les colonnes
+    Yapp = np.reshape(Yapp, (N,1))
+    Yapp_mat = Yapp.dot(np.ones((1,M))) # pour dupliquer Yapp dans toutes les colonnes
     Yapp_mat_sort = np.take_along_axis(Yapp_mat, sort_indices, axis=0)
     Yapp_mat_sort_tronque = Yapp_mat_sort[:K, : ]
-    #trouver comment avoir l'element le plus present de chaque colonne et après on aura Ypred
-    return ''
-    
+    Ypred = stats.mode(Yapp_mat_sort_tronque)[0][0] #permet d'avoir l'element le plus présent
+    return Ypred
+
+def evaluation_classifieur(Ytest, Ypred):
+    Ybool = (Ytest == Ypred)
+    nbr_true = sum(Ybool)
+    nbr_tot = len(Ybool)
+    return nbr_true/nbr_tot
+
+#%% Test
+
+K = 1000
+path = 'D:/julbr/Documents/ecole/ECL/3A\MOD 3.2 Deep Learning & IA/TD1/cifar-10-batches-py/data_batch_1'
+data_X, label_Y = lecture_cifar(path)
+Xapp, Yapp, Xtest, Ytest = decoupage_donnes(data_X, label_Y)
+dist = kppv_distances(Xtest, Xapp)
+Ypred = kppv_predict(dist, Yapp, K)
+accuracy = evaluation_classifieur(Ytest, Ypred)
+print(accuracy)
