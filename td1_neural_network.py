7c6c5190efa55c7619cfbf4767575190ed500960
Raphaël Chalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2020-11-03 19:43:40 +0100

    Update biaises weights

--- td1_neural_network.py
+++ td1_neural_network.py
@@ -10,6 +10,8 @@ Created on Tue Oct 20 10:40:01 2020
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
+import time
 
 seed = 2
 np.random.seed(seed) # pour que l'exécution soit déterministe
@@ -137,7 +139,7 @@ def passe_avant(X, W1, W2, b1, b2):
     O2 = np.divide(O2, np.sum(O2, axis=1)[:,np.newaxis], dtype="float32")
     return O1, O2
 
-def passe_arriere(X, Y_pred, Y, O1, O2, W1, W2, b1, b2, gradient_step):
+def passe_arriere(X, Y, O1, O2, W1, W2, b1, b2, gradient_step):
     grad_O2 = O2 - Y
     grad_I2 = d_sigmoid(O2)*grad_O2
     grad_W2 = O1.T.dot(grad_I2)
@@ -149,8 +151,11 @@ def passe_arriere(X, Y_pred, Y, O1, O2, W1, W2, b1, b2, gradient_step):
     W1 -= gradient_step*grad_W1
     W2 -= gradient_step*grad_W2
     
-    b1 -= gradient_step*grad_I1
-    b2 -= gradient_step*grad_I2
+    grad_b1 = np.sum(grad_I1, axis=0).reshape(b1.shape)
+    grad_b2 = np.sum(grad_I2, axis=0).reshape(b2.shape)
+    
+    b1 -= gradient_step*grad_b1
+    b2 -= gradient_step*grad_b2
         
     return W1, W2, b1, b2
 
@@ -186,10 +191,15 @@ def neural_network_donnees_aleatoires():
     # Initialisation aléatoire des poids du réseau
     W1 = 2*np.random.random((D_in, D_h)) - 1
     W1 = np.array(W1, dtype="float32")
-    b1 = np.zeros((1, D_h), dtype="float32")
+    
+    b1 = np.random.random((1, D_h))
+    b1 = np.array(b1, dtype="float32")
+    
     W2 = 2*np.random.random((D_h, D_out))-1
     W2 = np.array(W2, dtype="float32")
-    b2 = np.zeros((1, D_out), dtype="float32")
+    
+    b2 = np.random.random((1, D_out))
+    b2 = np.array(b2, dtype="float32")
     
     for i in range(nb_iterations):
         ####################################################
@@ -253,10 +263,11 @@ def neural_network_donnees_aleatoires():
 def neural_network_classification_cifar_10_train(Xapp,
                                                  Yapp,
                                                  gradient_step = 1e-3,
-                                                 nb_iterations = 10000,
-                                                 mini_batch_size = 10):
+                                                 nb_iterations = 1000,
+                                                 mini_batch_size = 100):
     
     np.random.seed(seed)
+    t0= time.time()
     
     # N est le nombre de données d'entrée
     # D_in est la dimension des données d'entrée
@@ -277,10 +288,16 @@ def neural_network_classification_cifar_10_train(Xapp,
     # Initialisation aléatoire des poids du réseau
     W1 = 2*np.random.random((D_in, D_h)) - 1
     W1 = np.array(W1, dtype="float32")
-    b1 = np.zeros((1, D_h), dtype="float32")
+    b1 = np.random.random((1, D_h))
+    b1 = np.array(b1, dtype="float32")
+    
     W2 = 2*np.random.random((D_h, D_out))-1
     W2 = np.array(W2, dtype="float32")
-    b2 = np.zeros((1, D_out), dtype="float32")
+    b2 = np.random.random((1, D_out))
+    b2 = np.array(b2, dtype="float32")
+    
+    best_param = [W1, W2, b1, b2]
+    best_accuracy = 0
     
     O1 = np.zeros((N, D_h), dtype="float32")
     O2 = np.zeros((N, D_out), dtype="float32")
@@ -295,7 +312,7 @@ def neural_network_classification_cifar_10_train(Xapp,
             borne_sup = (j+1)*mini_batch_size
             O1[borne_inf:borne_sup], O2[borne_inf:borne_sup] = passe_avant(X[borne_inf:borne_sup], W1, W2, b1, b2)
         if N%mini_batch_size > 0:
-            borne_inf = N%mini_batch_size
+            borne_inf = -N%mini_batch_size
             O1[borne_inf:], O2[borne_inf:] = passe_avant(X[borne_inf:], W1, W2, b1, b2)
         Y_pred = O2
         
@@ -305,16 +322,22 @@ def neural_network_classification_cifar_10_train(Xapp,
         loss = np.square(Y_pred - Y).sum()/2
         vect_loss[i] = loss
         
+        if i%100 == 0:
+            print("Itération", i, "loss :", loss)
+        
         ######################################################
         # Calcul et affichage de la précision du classifieur #
         ######################################################
         accuracy = evaluation_classifieur(Y, Y_pred)
         vect_accuracy[i] = accuracy
+        if accuracy > 1.05*best_accuracy:
+            best_accuracy = accuracy
+            best_param = copy.deepcopy([W1, W2, b1, b2])
         
         ########################################
         # Passe arrière : mis à jour des poids #
         ########################################
-        W1, W2, b1, b2 = passe_arriere(X, Y_pred, Y, O1, O2, W1, W2, b1, b2, gradient_step)
+        W1, W2, b1, b2 = passe_arriere(X, Y, O1, O2, W1, W2, b1, b2, gradient_step)
     
     vect_accuracy*= 100
     
@@ -338,23 +361,26 @@ def neural_network_classification_cifar_10_train(Xapp,
     ax2.grid()
     ax2.plot(vect_accuracy)
     
-    return Y_pred, W1, W2, vect_loss, vect_accuracy
+    execution_time = time.time() - t0
+    execution_time_minutes = execution_time//60
+    execution_time_seconds = execution_time%60
+    print("Training executed in {:n} minutes {:.0f} seconds.".format(execution_time_minutes, execution_time_seconds))
+    
+    return Y_pred, best_param, vect_loss, vect_accuracy
 
 def neural_network_classification_cifar_10_test(Xtest, 
                                                 Ytest, 
                                                 W1, 
-                                                W2, 
-                                                gradient_step=1e-3, 
+                                                W2,
+                                                b1,
+                                                b2,
+                                                gradient_step = 1e-3, 
                                                 nb_iterations = 1000):
     N, D_in = np.shape(Xtest)
     D_h, D_out = 20, 10
     
     # Création d'une matrice d'entrée X et de sortie Y
     X = np.array(Xtest/255, dtype="float32")  #Normalisation des données
-    
-    # Initialisation des poids du réseau
-    b1 = np.zeros((1, D_h))
-    b2 = np.zeros((1, D_out))
     
     ####################################################
     # Passe avant : calcul de la sortie prédite Y_pred #
@@ -374,69 +400,44 @@ def neural_network_classification_cifar_10_test(Xtest,
 # ATTENTION : Met plusieurs minutes à s'exécuter #
 ##################################################
 
-liste_gradient_step = [1e-2, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5]
+liste_gradient_step = [1e-2, 5e-2, 1e-3, 5e-3, 1e-4, 5e-4, 1e-5, 5e-5]
 nb_gradient_steps = len(liste_gradient_step)
 accuracy_matrix = np.zeros(nb_gradient_steps, dtype=float)
 
 Y = matrice_stochastique(Yapp)
 
 for i, g_step in enumerate(liste_gradient_step):
-    Y_pred, W1, W2, vect_loss, vect_accuracy = neural_network_classification_cifar_10_train(Xapp, Yapp, gradient_step = g_step)
+    Y_pred, best_param, vect_loss, vect_accuracy = neural_network_classification_cifar_10_train(Xapp, Yapp, gradient_step = g_step)
     accuracy = evaluation_classifieur(Y, Y_pred)
     accuracy *= 100
     accuracy_matrix[i] = accuracy
     print("Gradient step :", g_step)
-    print("Accuracy :", round(accuracy, 2), "%")
+    print("Accuracy :", round(accuracy, 2), "%\n")
 
-# accuracy_matrix
-# array([[10.4875, 10.025 , 10.025 , 10.025 ],
-#        [10.4875, 10.05  ,  9.625 , 10.025 ],
-#        [10.025 , 10.4875, 10.1625,  9.9   ],
-#        [22.05  , 21.375 , 20.4   , 27.875 ],
-#        [22.725 , 22.2875, 18.825 , 20.7125],
-#        [16.3625, 20.7   , 22.35  , 24.425 ],
-#        [16.7875, 18.1   , 18.7375, 21.55  ],
-#        [11.45  , 13.425 , 16.05  , 18.425 ]])
-# The gradient steps 1e-3 and 5e-4 appears to be the best.
+#array([10.275, 10.05 , 10.275, 10.475, 21.65 , 20.125, 23. ,21.45 , 16.725])
 
-#%% 
-gradient_step = 5e-4
-nb_iterations = 500
+#%% Entraînement du réseau et comparaison sur les données de validation
+# Temps d'exécution : 3 minutes
+gradient_step = 1e-3
+nb_iterations = 1000
 
+# Entraînement du réseau
 Yapp_stochastique = matrice_stochastique(Yapp)
-Y_pred, W1, W2, vect_loss, vect_accuracy = neural_network_classification_cifar_10_train(Xapp, Yapp, gradient_step=gradient_step, nb_iterations=nb_iterations)
+Y_pred, best_param, vect_loss, vect_accuracy = neural_network_classification_cifar_10_train(Xapp, Yapp, gradient_step=gradient_step, nb_iterations=nb_iterations)
 
-# fig = plt.figure()
-# ax1 = fig.add_subplot(211)
-# fig.suptitle("Fonction de perte en fonction des itérations")
-# ax1.set_xlabel(r'Itération $i$')
-# ax1.set_ylabel("Perte")
-# # ax1.set_xticks(np.arange(0, 25, step=2))
-# ax1.grid()
-# ax1.plot(vect_loss)
-
-# ax2 = fig.add_subplot(212)
-# fig.suptitle("Précision en fonction des itérations")
-# ax2.set_xlabel(r'Itération $i$')
-# ax2.set_ylabel("Précision")
-# # ax.set_xticks(np.arange(0, 25, step=2))
-# ax2.grid()
-# ax2.plot(vect_accuracy)
-
-# accuracy = evaluation_classifieur(Yapp_stochastique, Y_pred)
-# accuracy *= 100
-# print("Gradient step :", gradient_step, "and Nb iterations :", nb_iterations)
-# print("Accuracy :", round(accuracy, 2), "%")
-
+# Comparaison sur les données de validation
 Y_test_stochastique = matrice_stochastique(Ytest)
-Y_pred_test = neural_network_classification_cifar_10_test(Xtest, Ytest, W1, W2, gradient_step=gradient_step, nb_iterations=nb_iterations)
+best_W1, best_W2, best_b1, best_b2 = best_param
+Y_pred_test = neural_network_classification_cifar_10_test(Xtest, Ytest, best_W1, best_W2, best_b1, best_b2, gradient_step=gradient_step, nb_iterations=nb_iterations)
 accuracy_test = evaluation_classifieur(Y_test_stochastique, Y_pred_test)
 accuracy_test *= 100
 print("Gradient step :", gradient_step, "and Nb iterations :", nb_iterations)
 print("Maximum accuracy :", round(np.amax(vect_accuracy), 2), "% reached at iteration ", np.where(vect_accuracy == np.amax(vect_accuracy))[0][0])
 print("Accuracy test data:", round(accuracy_test, 2), "%")
 
-
+# Gradient step : 0.001 and Nb iterations : 1000
+# Maximum accuracy : 29.2 % reached at iteration  648
+# Accuracy test data: 20.0 %
 
 
 
